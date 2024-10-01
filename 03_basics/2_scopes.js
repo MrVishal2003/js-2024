29d22651d81e4a55213591a4a243023bccb3500d
Vishal Hadiyal <142768951+MrVishal2003@users.noreply.github.com>
2024-10-01 13:10:49 +0000

    done

--- 03_basics/2_scopes.js
+++ 03_basics/2_scopes.js
@@ -3,9 +3,56 @@ let a = 300
 if (true){
     let a =10
     const b = 20
-    console.log("Inner:", a);
+    // console.log("Inner:", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
+
+
+
+
+
+function one(){
+    const username = "vishal"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+one()
+
+if (true) {
+    const username = "vishal"
+    if(username === "vishal") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++ interesting ++++++++++
+
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1
+}
+
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5));
