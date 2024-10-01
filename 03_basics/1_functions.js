306d0b1e19e877f22106eae77029d196406435da
Vishal Hadiyal <142768951+MrVishal2003@users.noreply.github.com>
2024-10-01 11:51:03 +0000

    done

--- 03_basics/1_functions.js
+++ 03_basics/1_functions.js
@@ -27,7 +27,7 @@ const result = addTwoNumbers(3, 5)
 // console.log("Result:" ,result);
 
 
-function loginUserMessage(username = "Darshan"){
+function loginUserMessage(username){
     if(!username){
         console.log("Please Enter a Username");
         return
@@ -35,5 +35,36 @@ function loginUserMessage(username = "Darshan"){
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("vishal"));
 console.log(loginUserMessage("vishal"));
+// console.log(loginUserMessage("vishal"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "vishal",
+    price: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject ({
+    username: "darshan",
+    price: 400
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200,400,100,600]));
