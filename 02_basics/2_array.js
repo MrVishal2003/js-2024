0314f3263cfc98e3c9cbb75a819ed3e4147239b8
Vishal Hadiyal <142768951+MrVishal2003@users.noreply.github.com>
2024-10-05 07:37:23 +0000

    done

--- 02_basics/2_array.js
+++ 02_basics/2_array.js
@@ -16,13 +16,13 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-// console.log(real_another_array);
+console.log(real_another_array);
 
 
 
-console.log(Array.isArray("vishal"))
-console.log(Array.from("vishal"))
-console.log(Array.from({name: "vishal"})) // interesting
+// console.log(Array.isArray("vishal"))
+// console.log(Array.from("vishal"))
+// console.log(Array.from({name: "vishal"})) // interesting
 
 let score1 = 100
 let score2 = 200
---
+++ 03_basics/test.js
@@ -0,0 +1,5 @@
+const addNum = function (num1,num2){
+    console.log(num1 + num2);
+}
+addNum(1,3)
+// console.log(addNum(3,2));
